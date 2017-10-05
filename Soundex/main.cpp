e417dce493f654013257fd467ff726595f25620a
Brad Killeen <Brad@Lucass-MacBook-Pro.local>
2017-10-05 13:26:30 -0700

    Initial Commit

---
+++ Soundex/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Soundex
+//
+//  Created by Brad Killeen on 10/5/17.
+//  Copyright © 2017 Brad Killeen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
